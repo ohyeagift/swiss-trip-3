73208b58b0385c8cb30c9a1d2a43cba02eec098d
ohyeagift <ohyea.gift@gmail.com>
2026-09-11 14:23:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 // 1. 行程資料 (您可以隨時在這裡新增/修改行程)
 const travelData = {
     accommodations: {
-        "Luzern": { name: "琉森Airbnb, Bruchstrasse 35b", lat: 47.0497810810647, lng: 8.300931992893938 },
-        "Interlaken": { name: "因特拉肯Airbnb, Niesenstrasse 16", lat: 46.68289010140746, lng: 7.853981496756942 },
-        "Zermatt": { name: "策馬特 Haus Gornera, Bachstrasse 90", lat: 46.01801255700013, lng: 7.745654656757409 },
-        "Zurich": { name: "蘇黎世 Hotel City Zürich, Löwenstrasse 34", lat: 47.37502358530974, lng: 8.53643189805861 }
+        "Luzern": { name: "琉森Airbnb, Bruchstrasse 35b", query: "Bruchstrasse 35b, Luzern", lat: 47.0485, lng: 8.3000 },
+        "Interlaken": { name: "因特拉肯Airbnb, Niesenstrasse 16", query: "Niesenstrasse 16, 3800 Interlaken", lat: 46.6830, lng: 7.8540 },
+        "Zermatt": { name: "策馬特 Haus Gornera, Bachstrasse 90", query: "Haus Gornera, Bachstrasse 90, 3920 Zermatt", lat: 46.0235, lng: 7.7490 },
+        "Zurich": { name: "蘇黎世 Hotel City Zürich", query: "Hotel City Zürich, Löwenstrasse 34", lat: 47.3755, lng: 8.5375 }
     },
     daily_itinerary: [
         {
             day_id: "DAY 1", date: "10/16 (五)", route_title: "蘇黎世 ➔ 琉森", accommodation: "Luzern",
             activities: [
-                { time: "07:30", activity: "抵達蘇黎世機場", lat: 47.4582, lng: 8.5555 },
-                { time: "上午", activity: "搭乘火車 Zurich HB to Luzern", lat: 47.3781, lng: 8.5401 },
+                { time: "07:30", activity: "抵達蘇黎世機場", query: "Zurich Airport", lat: 47.4582, lng: 8.5555 },
+                { time: "上午", activity: "搭乘火車 Zurich HB to Luzern", query: "Zurich HB", lat: 47.3781, lng: 8.5401 },
                 { 
                     time: "下午", activity: "Pilatus 皮拉圖斯峰", altitude: "2,128m", lat: 46.9795, lng: 8.2555,
                     links: { website: "https://www.pilatus.ch/", webcam: "https://www.pilatus.ch/en/discover/pilatus-live", weather: "https://www.meteoswiss.admin.ch/local-forecasts/pilatus/6010.html" }
                 },
-                { time: "傍晚", activity: "Luzern City Walk 琉森市區漫步", lat: 47.0501, lng: 8.3093 }
+                { time: "傍晚", activity: "Luzern City Walk 琉森市區漫步", query: "Luzern", lat: 47.0501, lng: 8.3093 }
             ]
         },
         {
             day_id: "DAY 2", date: "10/17 (六 )", route_title: "琉森 ➔ 鐵力士峰", accommodation: "Luzern",
             activities: [
-                { time: "09:10", activity: "Luzern to Engelberg", lat: 46.8200, lng: 8.4020 },
+                { time: "09:10", activity: "Luzern to Engelberg", query: "Luzern Bahnhof", lat: 46.8200, lng: 8.4020 },
                 { 
                     time: "上午", activity: "Titlis 鐵力士峰", altitude: "3,238m", lat: 46.7720, lng: 8.4260,
                     links: { website: "https://www.titlis.ch/" }
                 }
             ]
         },
-         {
-            day_id: "DAY 3", date: "10/18 (日)", route_title: "琉森 ➔ 因特拉肯", accommodation: "Interlaken",
+        {
+            day_id: "DAY 3", date: "10/18 (日 )", route_title: "琉森 ➔ 因特拉肯", accommodation: "Interlaken",
             activities: [
-                { time: "早上", activity: "Church", lat: 47.0501, lng: 8.3093 },
+                { time: "早上", activity: "Church", query: "Church, Luzern", lat: 47.0501, lng: 8.3093 },
                 { 
                     time: "上午", activity: "Rigi 瑞吉山 齒軌火車", altitude: "1,798m", lat: 47.0566, lng: 8.4846,
                     links: { website: "https://www.rigi.ch/" }
                 },
-                { time: "14:00", activity: "Airbnb 拎行李", lat: 47.0485, lng: 8.3000 },
-                { time: "15:06", activity: "搭乘黃金列車前往 Interlaken West", lat: 46.6814, lng: 7.8513 },
-                { time: "傍晚", activity: "酒店 Check-in", lat: 46.6830, lng: 7.8540 }
+                { time: "14:00", activity: "Airbnb 拎行李", query: "Bruchstrasse 35b, Luzern", lat: 47.0485, lng: 8.3000 },
+                { time: "15:06", activity: "搭乘黃金列車前往 Interlaken West", query: "Interlaken West", lat: 46.6814, lng: 7.8513 },
+                { time: "傍晚", activity: "酒店 Check-in", query: "Niesenstrasse 16, 3800 Interlaken", lat: 46.6830, lng: 7.8540 }
             ]
         },
         {
@@ -50,7 +50,7 @@ const travelData = {
                     time: "中午", activity: "Jungfraujoch 少女峰", altitude: "3,454m", lat: 46.5475, lng: 7.9826,
                     links: { website: "https://www.jungfrau.ch/", webcam: "https://www.jungfrau.ch/en-gb/live/webcams/" }
                 },
-                { time: "下午", activity: "餐廳叫芝士火鍋 & 叫朱古力", lat: 46.6814, lng: 7.8513 }
+                { time: "下午", activity: "餐廳叫芝士火鍋 & 叫朱古力", query: "Interlaken", lat: 46.6814, lng: 7.8513 }
             ]
         },
         {
@@ -73,11 +73,11 @@ const travelData = {
                 }
             ]
         },
-         {
-            day_id: "DAY 7", date: "10/22 (四)", route_title: "伯恩 ➔ 施皮茨", accommodation: "Interlaken",
+        {
+            day_id: "DAY 7", date: "10/22 (四 )", route_title: "伯恩 ➔ 施皮茨", accommodation: "Interlaken",
             activities: [
-                { time: "上午", activity: "Bern 伯恩 (首都半日遊)", lat: 46.9480, lng: 7.4474 },
-                { time: "下午", activity: "Spiez 施皮茨", lat: 46.6894, lng: 7.6800 },
+                { time: "上午", activity: "Bern 伯恩 (首都半日遊)", query: "Bern", lat: 46.9480, lng: 7.4474 },
+                { time: "下午", activity: "Spiez 施皮茨", query: "Spiez", lat: 46.6894, lng: 7.6800 },
                 { 
                     time: "傍晚", activity: "Harder Kulm 哈德昆觀景台", altitude: "1,322m", lat: 46.6975, lng: 7.8647,
                     links: { website: "https://www.jungfrau.ch/en-gb/harder-kulm/" }
@@ -87,16 +87,16 @@ const travelData = {
         {
             day_id: "DAY 8", date: "10/23 (五 )", route_title: "因特拉肯 ➔ 策馬特", accommodation: "Zermatt",
             activities: [
-                { time: "07:00", activity: "寄2件行李去 Zurich", lat: 46.6814, lng: 7.8513 },
-                { time: "08:04", activity: "Interlaken West to Zermatt", lat: 46.0207, lng: 7.7491 },
-                { time: "下午", activity: "酒店 Check-in", lat: 46.0235, lng: 7.7490 },
-                { time: "傍晚", activity: "Zermatt City Walk", lat: 46.0190, lng: 7.7460 }
+                { time: "07:00", activity: "寄2件行李去 Zurich", query: "Interlaken West", lat: 46.6814, lng: 7.8513 },
+                { time: "08:04", activity: "Interlaken West to Zermatt", query: "Zermatt Bahnhof", lat: 46.0207, lng: 7.7491 },
+                { time: "下午", activity: "酒店 Check-in", query: "Bachstrasse 90, 3920 Zermatt", lat: 46.0235, lng: 7.7490 },
+                { time: "傍晚", activity: "Zermatt City Walk", query: "Zermatt", lat: 46.0190, lng: 7.7460 }
             ]
         },
         {
             day_id: "DAY 9", date: "10/24 (六)", route_title: "策馬特 ➔ 冰川天堂", accommodation: "Zermatt",
             activities: [
-                { time: "早上", activity: "Church", lat: 46.0190, lng: 7.7460 },
+                { time: "早上", activity: "Church", query: "Church, Zermatt", lat: 46.0190, lng: 7.7460 },
                 { 
                     time: "上午", activity: "Gornergrat 觀景台", altitude: "3,089m", lat: 45.9838, lng: 7.7854,
                     links: { website: "https://www.gornergrat.ch/" }
@@ -111,20 +111,19 @@ const travelData = {
         {
             day_id: "DAY 10", date: "10/25 (日 )", route_title: "策馬特 ➔ 蘇黎世", accommodation: "Zurich",
             activities: [
-                { time: "早上", activity: "Church", lat: 46.0190, lng: 7.7460 },
-                { time: "07:28", activity: "Zermatt to Zurich", lat: 47.3781, lng: 8.5401 },
-                { time: "下午", activity: "蘇黎世 City Walk", lat: 47.3710, lng: 8.5410 }
+                { time: "早上", activity: "Church", query: "Church, Zurich", lat: 46.0190, lng: 7.7460 },
+                { time: "07:28", activity: "Zermatt to Zurich", query: "Zurich HB", lat: 47.3781, lng: 8.5401 },
+                { time: "下午", activity: "蘇黎世 City Walk", query: "Zurich", lat: 47.3710, lng: 8.5410 }
             ]
         },
         {
             day_id: "DAY 11", date: "10/26 (一)", route_title: "蘇黎世 ➔ 香港", accommodation: "",
             activities: [
-                { time: "早上", activity: "Zurich HB to Zurich Flughafen", lat: 47.4582, lng: 8.5555 },
-                { time: "上午", activity: "準備去機場", lat: 47.4582, lng: 8.5555 },
-                { time: "11:55", activity: "搭乘 CX382 航班返回香港", lat: 47.4582, lng: 8.5555 }
+                { time: "早上", activity: "Zurich HB to Zurich Flughafen", query: "Zurich Airport", lat: 47.4582, lng: 8.5555 },
+                { time: "上午", activity: "準備去機場", query: "Zurich Airport", lat: 47.4582, lng: 8.5555 },
+                { time: "11:55", activity: "搭乘 CX382 航班返回香港", query: "Zurich Airport", lat: 47.4582, lng: 8.5555 }
             ]
         }
-        // 您可以繼續依照這個格式補齊其他天數的資料...
     ]
 };
 
@@ -133,7 +132,7 @@ let map;
 let markers = [];
 let currentDayIndex = 0;
 
-// 3. 初始化 Google Maps (由 HTML 中的 callback 觸發 )
+// 3. 初始化 Google Maps (由 HTML 中的 callback 觸發)
 async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
     
@@ -180,8 +179,10 @@ async function loadDay(index) {
     timelineContainer.innerHTML = '';
 
     dayData.activities.forEach((act, i) => {
-        // 已修改：將 dir (導航) 改為 search (單純顯示地圖位置)，並將文字改為「地圖」
-        let linksHtml = `<a href="https://www.google.com/maps/search/?api=1&query=${act.lat},${act.lng}" target="_blank" class="link-btn">📍 地圖</a>`;
+        // 【修改重點】：優先使用 act.query，如果沒有則使用 act.activity 作為搜尋字詞
+        const searchQuery = encodeURIComponent(act.query || act.activity);
+        let linksHtml = `<a href="https://www.google.com/maps/search/?api=1&query=${searchQuery}" target="_blank" class="link-btn">📍 地圖</a>`;
+        
         if (act.links ) {
             if (act.links.website) linksHtml += `<a href="${act.links.website}" target="_blank" class="link-btn">🌐 官網</a>`;
             if (act.links.webcam) linksHtml += `<a href="${act.links.webcam}" target="_blank" class="link-btn">📷 攝影機</a>`;
@@ -205,7 +206,9 @@ async function loadDay(index) {
     // 加入住宿卡片
     if (dayData.accommodation && travelData.accommodations[dayData.accommodation]) {
         const acc = travelData.accommodations[dayData.accommodation];
-        // 已修改：在住宿卡片中加入地圖按鈕，並微調對齊方式
+        // 【修改重點】：住宿卡片也使用 query 或 name 進行搜尋
+        const accSearchQuery = encodeURIComponent(acc.query || acc.name);
+        
         timelineContainer.innerHTML += `
             <div class="accommodation-card" style="align-items: flex-start;">
                 <div class="acc-icon" style="margin-top: 2px;">🏠</div>
@@ -213,7 +216,7 @@ async function loadDay(index) {
                     <div class="acc-title">今晚住宿</div>
                     <div class="acc-name">${acc.name}</div>
                     <div class="links">
-                        <a href="https://www.google.com/maps/search/?api=1&query=${acc.lat},${acc.lng}" target="_blank" class="link-btn">📍 地圖</a>
+                        <a href="https://www.google.com/maps/search/?api=1&query=${accSearchQuery}" target="_blank" class="link-btn">📍 地圖</a>
                     </div>
                 </div>
             </div>
